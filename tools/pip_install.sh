cf4723b13a8936bc2fa02ea62a93ae726091ff36
Duk Loi <duk@tesora.com>
2016-05-09 11:32:58 -0400

    Fix gate by pinning horizon to stable/mitaka
    EE-1.8 is stable/mitaka and the horizon version for the tests should
    be pinned to stable/mitaka as well.
    
    Change-Id: I1ab8e7392fcbd9ac8c28ac040d3f8203dcfbcfde

--- tools/pip_install.sh
+++ tools/pip_install.sh
@@ -11,6 +11,13 @@ uninstall_cmd="pip uninstall -y"
 # install all pip libraries from pypi first
 $install_cmd -U $*
 
+# remove the horizon installed from source (can't get rid of it in test-requirements.txt due to global requirements conflict
+$uninstall_cmd horizon
+
+# TODO: replace the hardcoded branch with env variable
+HORIZON_PIP_LOCATION="git://github.com/openstack/horizon.git@stable/mitaka#egg=horizon"
+$install_cmd -U -e ${HORIZON_PIP_LOCATION}
+
 # remove the python-troveclient from pypi
 $uninstall_cmd python-troveclient
 
